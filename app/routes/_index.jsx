9cc873469c572b093455826bbba8a16462019a8d
Deepak-rasamsetty <deepakrasamsetty92@gmail.com>
2024-01-10 18:19:19 +0530

    deleted redeundant code

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -14,75 +14,33 @@ import {
 export const meta = () => {
   return [{ title: "New Remix App" }];
 };
-const stackItems= <Stack
-direction="row"
-divider={<Divider orientation="vertical" flexItem />}
-spacing={2}
-width={800}
->
-<Card
-    sx={{ width: 150, paddingLeft: "20px", paddingRight: "20px" }}
-  >
-    <CardActionArea>
-      <CardMedia component="img" height="200" image="document.png" />
-      <CardContent>
-        <span className="justify-center">
-          <h2>Submit Application</h2>
-        </span>{" "}
-      </CardContent>
-    </CardActionArea>
-  </Card>
- <Card
-    sx={{ width: 150, paddingLeft: "20px", paddingRight: "20px" }}
-  >
-    <CardActionArea>
-      <CardMedia component="img" height="200" image="transaction.png" />
-      <CardContent>
-        <span className="justify-center">
-          <h2>submit Transaction</h2>
-        </span>{" "}
-      </CardContent>
-    </CardActionArea>
-  </Card> 
- <Card
-    sx={{ width: 150, paddingLeft: "20px", paddingRight: "20px" }}
-  >
-    <CardActionArea>
-      <CardMedia component="img" height="200" image="lookup.png" />
-      <CardContent>
-        <span className="justify-center">
-          <h2>Lookup Account</h2>
-        </span>{" "}
-      </CardContent>
-    </CardActionArea>
-  </Card> 
-</Stack>;
-
 export default function Index() {
   return (
     <div style={{ height: "60%" }}>
-    
-
-     
       <Container maxWidth="sm" style={{ marginTop: "5%" }}>
-       {/* {stackItems} */}
         <Grid container spacing={12} sx={{ flexGrow: 1 }}>
           <Grid item xs={4} justifyContent="center">
-            <Link href='/application/newApplication'>
-            <Card
-              sx={{ width: 140, paddingLeft: "20px", paddingRight: "20px" }}
+            <Link
+              href="/application/newApplication"
+              style={{ color: "black", textDecoration: "none" }}
             >
-              <CardActionArea>
-                <CardMedia component="img" height="200" image="document.png" />
-                <CardContent>
-                  <span className="justify-center">
-                    <h2>Submit Application</h2>
-                  </span>{" "}
-                </CardContent>
-              </CardActionArea>
-            </Card>
+              <Card
+                sx={{ width: 140, paddingLeft: "20px", paddingRight: "20px" }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="document.png"
+                  />
+                  <CardContent>
+                    <span className="justify-center">
+                      <h2>Submit Application</h2>
+                    </span>{" "}
+                  </CardContent>
+                </CardActionArea>
+              </Card>
             </Link>
-            
           </Grid>
           <Grid item xs={4} justifyContent="center">
             <Card
